0587d257b00c229430225f254928b8f9de0e7cc8
Lukasz <lczapiga@outlook.com>
2016-02-29 18:43:49 +0000

    Basic filter and data display

--- public/js/controllers/visualisationsController.js
+++ public/js/controllers/visualisationsController.js
@@ -12,31 +12,37 @@ indexController.controller('visualisationsCtrl', function ($scope, openDataServi
         var agencyTotalCosts = 0;
         var count = 0;
 
-        for(i=0;i<$scope.data.length; i++){
+        for(i=0; i<$scope.data.length; i++){
             count++;
             var agency = {};
             var agencyName = $scope.data[i]['Agency Name'];
             var temp;
-            var first;
-
-            if(agencyName === $scope.data[1]['Agency Name']){
-                temp = agencyName;
-            }
-
-            lifecycleTotalCosts = lifecycleTotalCosts + $scope.data[i]['Lifecycle Cost'];
-            agencyTotalCosts = agencyTotalCosts +  $scope.data[i]['Projected/Actual Cost ($ M)'];
-
 
             if(agencyName !== prev ){
                 agency.name = prev;
                 agency.totalCosts = agencyTotalCosts;
                 agency.lifecycleCosts = lifecycleTotalCosts;
                 agency.projectCount = count;
+
                 $scope.agencyData.push(agency);
+
                 count = 0;
                 lifecycleTotalCosts = 0;
                 agencyTotalCosts = 0;
+            }else {
+                lifecycleTotalCosts = lifecycleTotalCosts + $scope.data[i]['Lifecycle Cost'];
+                agencyTotalCosts = agencyTotalCosts +  $scope.data[i]['Projected/Actual Cost ($ M)'];
             }
+
+            if(i === $scope.data.length-1)
+            {
+                agency.name = agencyName;
+                agency.totalCosts = agencyTotalCosts;
+                agency.lifecycleCosts = lifecycleTotalCosts;
+                agency.projectCount = count+1;
+                $scope.agencyData.push(agency);
+            }
+
             prev = agencyName;
         }
         console.log("ok");
